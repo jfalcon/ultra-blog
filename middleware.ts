e6bbd296d68a934717a9d1d4630ca767076c74bb
Jeremy Falcon <jfalcon@users.noreply.github.com>
2025-05-30 13:05:58 -0500

    docs: ab test

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,11 @@ const i18nMiddleware = createMiddleware(routing);
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 export default async function middleware(request: NextRequest) {
+  // this is where you'd perform A/B (split) testing, let's say
+  // split testing is IP-based, the request object has that info
+  // if (request.nextUrl.pathname.startsWith('/about')) {
+  //   return NextResponse.rewrite(new URL('/about-2', request.url))
+  // }
   return i18nMiddleware(request);
 }
 
